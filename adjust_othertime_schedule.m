839d9fc4c25f6778963aa6b970a0b8352c1775db
shaopf <shaopf@buaa.edu.cn>
2022-12-03 14:59:01 +0800

    MP-GA 基线softmax 修复LFT 活动员工目标

--- adjust_othertime_schedule.m
+++ adjust_othertime_schedule.m
@@ -1,20 +1,20 @@
-function [temp_variables, conflict_acts_info] = adjust_othertime_schedule(project_para,data_set, iter_variables, conflict_acts_info,performing_acts_infos, forward_set,need_global_activity,cpm, time)
-temp_variables = iter_variables;
-%5.3.1  确定当前时刻需要全局资源的冲突活动列表  cur_need_global_activity
-cur_need_global_activity = find_cur_need_global_activity(iter_variables, need_global_activity, time, iter_variables.allocated_set); % 当前时刻需要全局资源的活动
-slst = find_slst(project_para, data_set, cpm, iter_variables); %找松弛时间
-%5.3.2  根据cur_need_global_activity确定冲突活动顺序列表
-%cur_conflict(按照项目权重、活动工期、全局需求量三个优先规则)
-if ~isempty(cur_need_global_activity)
-    [cur_conflict] = find_cur_conflict_repair(data_set, iter_variables, cur_need_global_activity, slst);
-     [temp_variables, conflict_act_info] = adjust_othertime_allocate_resource(data_set, iter_variables,performing_acts_infos, cur_conflict, time);
-    %%  局部更新update_clpex_option (优先关系约束及资源约束进行局部更新）
-    %6.1  通过紧前活动的最大完成时间--确定未安排活动开始时间
-    temp_variables = reschedule_local_time(temp_variables, forward_set, time);
-    %6.2  确定未安排活动满足资源约束
-    temp_variables = reschedule_local_resource(project_para, data_set, temp_variables, time);
-    % temp_total_duration = max(temp2_local_end_times, [], 2);  % 每个项目工期=每个项目的活动结束时间的最大值2行*1列
-    %6.3  传递
-    %             iter_variables = temp_variables;
-    conflict_acts_info{time} = conflict_act_info;
-end
+function [temp_variables, conflict_acts_info] = adjust_othertime_schedule(project_para, data_set, iter_variables, conflict_acts_info, performing_acts_infos, forward_set, need_global_activity, cpm, time)
+    temp_variables = iter_variables;
+    %5.3.1  确定当前时刻需要全局资源的冲突活动列表  cur_need_global_activity
+    cur_need_global_activity = find_cur_need_global_activity(iter_variables, need_global_activity, time, iter_variables.allocated_set); % 当前时刻需要全局资源的活动
+    lft = find_lft(project_para, data_set, cpm, iter_variables); %最晚开始时间
+    %5.3.2  根据cur_need_global_activity确定冲突活动顺序列表
+    %cur_conflict(按照项目权重、活动工期、全局需求量三个优先规则)
+    if ~isempty(cur_need_global_activity)
+        [cur_conflict] = find_cur_conflict_repair(data_set, iter_variables, cur_need_global_activity, lft);
+        [temp_variables, conflict_act_info] = adjust_othertime_allocate_resource(data_set, iter_variables, performing_acts_infos, cur_conflict, time);
+        %%  局部更新update_clpex_option (优先关系约束及资源约束进行局部更新）
+        %6.1  通过紧前活动的最大完成时间--确定未安排活动开始时间
+        temp_variables = reschedule_local_time(temp_variables, forward_set, time);
+        %6.2  确定未安排活动满足资源约束
+        temp_variables = reschedule_local_resource(project_para, data_set, temp_variables, time);
+        % temp_total_duration = max(temp2_local_end_times, [], 2);  % 每个项目工期=每个项目的活动结束时间的最大值2行*1列
+        %6.3  传递
+        %             iter_variables = temp_variables;
+        conflict_acts_info{time} = conflict_act_info;
+    end
---
+++ find_lft.m
@@ -0,0 +1,45 @@
+function [lft] = find_lft(project_para, data_set, cpm, iter_variables)
+
+    % used parameters
+    num_j = project_para.num_j;
+    L = project_para.L;
+
+    E = data_set.E;
+    ad = data_set.ad;
+
+    cpm_start_time = cpm.start_time;
+
+    iter_d = iter_variables.d;
+    iter_local_end_times = iter_variables.local_end_times;
+
+    est = zeros(L, num_j);
+
+    for i = 1:L
+        est(i, :) = cpm_start_time(i, :) + ad(i) + 1;
+    end
+
+    lst = zeros(L, num_j); %预生成各个活动的最晚开始时间
+    lft = zeros(L, num_j); %预生成各个活动的最晚开始时间
+    slst = zeros(L, num_j); %预生成各个活动的松弛时间
+
+    for i = 1:L
+        lst(i, num_j) = max(iter_local_end_times (i, :)); %最后的虚拟活动的活动工期,改
+        slst(i, num_j) = 0; %虚拟活动的松弛时间为0
+
+        for activity_choose = num_j - 1:-1:1
+            sucdecessors = E(activity_choose, :, i); % % %补充所选活动的紧后活动集合
+            sucdecessors(find(sucdecessors == 0)) = [];
+
+            if length(sucdecessors) == 1
+                lst(i, activity_choose) = lst(i, sucdecessors) -iter_d(activity_choose, :, i);
+            else
+                lst(i, activity_choose) = min(lst(i, sucdecessors)) - iter_d (activity_choose, :, i);
+            end
+
+            lft(activity_choose) = min(lst(sucdecessors));
+            slst (i, activity_choose) = lst(i, activity_choose) - est(i, activity_choose);
+        end
+
+    end
+
+end
